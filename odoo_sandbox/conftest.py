c71124321874d0084e3c2a7a69dfab44e6f16eee
kiarieking <kiariekevin22@gmail.com>
2025-10-09 07:51:46 +0000

    refactored conftest and requirements.txt

--- odoo_sandbox/conftest.py
+++ odoo_sandbox/conftest.py
@@ -20,7 +20,7 @@ def driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument(f"--user-data-dir={temp_profile}")
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
     

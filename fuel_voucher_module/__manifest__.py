62418b8dd7483b0829ffa52f7fffe2e8d4544a3d
kiarieking <kiariekevin22@gmail.com>
2026-01-12 12:56:39 +0000

    Jenkins_test: Push to test the jenkins webhook on remote repo

--- fuel_voucher_module/__manifest__.py
+++ fuel_voucher_module/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Fuel Management",
 
-    'summary': """Module to allow users to record fuel vouchers.""",
+    'summary': """Module to allow users to record fuel vouchers. test change 1""",
 
     'description': """Module to allow users to record fuel vouchers.""",
 

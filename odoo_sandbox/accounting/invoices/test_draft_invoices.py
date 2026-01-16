7229c5d7aacb93d42ac37cb329fe662a070dc657
kiarieking <kiariekevin22@gmail.com>
2026-01-16 12:01:50 +0000

    [ADD] Accounting dynamic open invoices

--- odoo_sandbox/accounting/invoices/test_draft_invoices.py
+++ odoo_sandbox/accounting/invoices/test_draft_invoices.py
@@ -17,7 +17,7 @@ def test_confirm_invoice(driver,login,accounting_icon):
     group_invoices(driver)
     status = "Draft"
     invoice_no = "INV/2023/0844"
-    open_invoices(driver,status,invoice_no) 
+    open_invoices(driver,status) 
     confirm_invoice(driver)
     time.sleep(3)
 
@@ -27,8 +27,8 @@ def test_preview_invoice(driver,login,accounting_icon):
     accounting_icon()
     group_invoices(driver)
     status = "Draft"
-    invoice_no = "INV/2023/0844"
-    open_invoices(driver,status,invoice_no)
+    # invoice_no = "INV/2023/0844"
+    open_invoices(driver,status)
     preview_invoice(driver)
     time.sleep(3)
 
@@ -39,7 +39,7 @@ def test_cancel_invoice(driver,login,accounting_icon):
     group_invoices(driver)
     status = "Draft"
     invoice_no = "INV/2023/0844"
-    open_invoices(driver,status,invoice_no)
+    open_invoices(driver,status)
     cancel_invoice(driver)
     time.sleep(3)
 
@@ -60,14 +60,39 @@ def group_invoices(driver):
     status = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//span[@role='menuitemcheckbox' and normalize-space()='Status']")))
     status.click()
 
-def open_invoices(driver,status,invoice_no):
+def open_invoices(driver,status):
+    print(">>> USING NEW open_invoices <<<")
     status_xpath = f"//th[@class='o_group_name' and contains(normalize-space(), '{status}')]"
     invoice_grp = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,status_xpath)))
     invoice_grp.click()
-    # invoice_xpath = f"//td[@name='name' and normalize-space()='{invoice_no}']"
-    invoice_xpath = "//tbody/tr[contains(@class,'o_data_row')][1]"
-    invoice = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,invoice_xpath)))
+    wait = WebDriverWait(driver, 20)
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//tbody//tr[contains(@class,'o_data_row')]")
+        )
+    )
+
+    first_invoice_xpath = (
+        "(//tbody//tr[contains(@class,'o_data_row')]"
+        "//td[@name='name'])[1]"
+    )
+
+    # WAIT: element is visible (NOT clickable)
+    invoice = wait.until(
+        EC.visibility_of_element_located((By.XPATH, first_invoice_xpath))
+    )
     invoice.click()
+
+    # Scroll into view (critical for Odoo)
+    #driver.execute_script(
+        # "arguments[0].scrollIntoView({block:'center'});", invoice
+    # )
+
+    # Click with JS fallback
+    # try:
+    #     invoice.click()
+    # except Exception:
+    #     driver.execute_script("arguments[0].click();", invoice)
 
 def confirm_invoice(driver):
     confirm_btn = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//button[.//span[normalize-space()='Confirm']]")))
